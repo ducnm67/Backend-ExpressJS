5db263c34dd0796b1a87aecdc7827f188a4696cd
Minh DUX <20021519@vnu.edu.vn>
2024-08-13 18:07:34 +0700

    Using MySQL with Node.JS

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const configViewEngine = require('./config/viewEngine');
 const webRouter = require('./routes/web');
+const mysql = require('mysql2');
 
 const app = express();
 
@@ -15,6 +16,19 @@ configViewEngine(app);
 
 //config routes
 app.use('/', webRouter);
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307,
+  user: 'root',
+  password: '123456',
+  database: 'hoidanit',
+});
+
+connection.query('select * from Users;', function (err, results, fields) {
+  console.log('>>>results= ', results); // results contains rows returned by server
+  console.log('>>>fields= ', fields); // fields contains extra meta data about results, if available
+});
 
 //run app
 app.listen(port, hostname, () => {
